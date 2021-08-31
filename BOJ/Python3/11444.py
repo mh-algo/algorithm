cf367bd2a9fbec5f9612132e53ffe8ca2b036057
mh-algo <84570976+mh-algo@users.noreply.github.com>
2021-08-31 20:13:57 +0900

    11444
    11444

---
+++ BOJ/Python3/11444.py
@@ -0,0 +1,34 @@
+# boj 2749 와 동일
+
+n=int(input())
+p=1000000007
+
+def square_matrix_mul(a,b):
+	size=len(a)
+	arr=[[0]*size for _ in range(size)]
+	for i in range(size):
+		for j in range(size):
+			for k in range(size):
+				arr[i][j]+=a[i][k]*b[k][j]
+			arr[i][j]%=p
+	return arr
+
+def matrix_pow(arr,n):
+	if n==1:
+		return arr
+	else:
+		tmp=matrix_pow(arr,n//2)
+		mul=square_matrix_mul(tmp,tmp)
+		if n%2==0:
+			return mul
+		else:
+			return square_matrix_mul(mul,arr)
+
+def fibo(n):
+	arr=[[1,1],[1,0]]
+	if n==0:
+		return 0
+	ret=matrix_pow(arr,n)
+	return ret[0][1]
+
+print(fibo(n))
